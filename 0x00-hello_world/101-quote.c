f5c5c1b32fae66d70b83d971be10866ca0cd6d1f
Moonlight0310 <aguejoufsamia@gmail.com>
2023-03-16 16:33:23 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,11 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 1 (Success)
+ * main - Prints a string to the standard error
+ * Return: 1 (Error)
  */
 int main(void)
 {
-    char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-    write(STDERR_FILENO, str, 59);
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
     return (1);
 }
