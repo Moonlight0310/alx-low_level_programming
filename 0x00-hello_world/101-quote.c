8f9d12b65363cd819a2313018f8139890816e0ff
Moonlight0310 <aguejoufsamia@gmail.com>
2023-03-16 16:22:47 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 
 /**
- * main - A C program that prints a line to the standard error
+ * main - A C program that prints a line to the standard output
  * Return: 1 (Error)
  */
 int main(void)
 {
-    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-    return (1);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
